31f3a9ee1f7e36ad50c76c4704c52e0a90f64995
Laurent Tran <ltran@et.esiea.fr>
2023-01-08 03:13:32 +0100

    first part working

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+
+
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgreslolo
+export POSTGRES_PASSWORD=laurent123
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=virtucontainer
+export AWS_BUCKET=udagram-esiea-laurenttran
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
